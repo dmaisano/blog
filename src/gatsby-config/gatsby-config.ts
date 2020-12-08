6fb5e74f5517b7844dec626d98be8a1debf6aef1
Domenico Maisano <dmaisanooo@gmail.com>
2020-12-08 03:36:56 -0500

    Added gatsby transformer remark and mdx plugins; Added GraphQL Playground

--- src/gatsby-config/gatsby-config.ts
+++ src/gatsby-config/gatsby-config.ts
@@ -1,3 +1,20 @@
+import dotenv from "dotenv"
+import { join } from "path"
+import { existsSync, openSync } from "fs"
+
+const NODE_ENV = process.env.NODE_ENV as "development" | "production"
+const ENV_FILE_PATH = join(__dirname, `../../.env.${NODE_ENV}`)
+
+try {
+  if (!existsSync(ENV_FILE_PATH)) {
+    openSync(ENV_FILE_PATH, "w")
+  }
+} catch (err) {
+  console.log(err)
+}
+
+dotenv.config({ path: ENV_FILE_PATH })
+
 export default {
   siteMetadata: {
     title: `dmaisano`,
@@ -7,6 +24,8 @@ export default {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-mdx`,
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
@@ -33,46 +52,3 @@ export default {
     // `gatsby-plugin-offline`,
   ],
 }
-
-// const withDefaults = require(`./src/utils/default-options`)
-
-// module.exports = (themeOptions) => {
-//   const options = withDefaults(themeOptions)
-//   const { mdx = false } = themeOptions
-
-//   return {
-//     siteMetadata: {
-//       title: `dmaisano`,
-//       siteTitle: `dmaisano`,
-//       description: `Personal Blog`,
-//       author: `@dmaisano`,
-//     },
-//     plugins: [
-//       `gatsby-plugin-react-helmet`,
-//       // {
-//       //   resolve: `gatsby-source-filesystem`,
-//       //   options: {
-//       //     name: `images`,
-//       //     path: `${__dirname}/src/images`,
-//       //   },
-//       // },
-//       // `gatsby-transformer-sharp`,
-//       // `gatsby-plugin-sharp`,
-//       {
-//         resolve: `gatsby-plugin-manifest`,
-//         options: {
-//           name: `gatsby-starter-default`,
-//           short_name: `starter`,
-//           start_url: `/`,
-//           background_color: `#663399`,
-//           theme_color: `#663399`,
-//           display: `minimal-ui`,
-//           icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-//         },
-//       },
-//       // this (optional) plugin enables Progressive Web App + Offline functionality
-//       // To learn more, visit: https://gatsby.dev/offline
-//       // `gatsby-plugin-offline`,
-//     ],
-//   }
-// }
---
+++ src/gatsby-config/site-options.ts
@@ -0,0 +1,13 @@
+export default {
+  basePath: "/",
+  blogPath: "/posts",
+  postsPath: "content/posts",
+  postsPrefix: "/",
+  pagesPath: "content/pages",
+  tagsPath: "/tags",
+  externalLinks: [],
+  navigation: [],
+  showLineNumbers: "",
+  showCopyButton: "",
+  formatString: "",
+}

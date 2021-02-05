b3e4efad31c6a6a5c621ca98d7bded68c1849131
Domenico Maisano <dmaisanooo@gmail.com>
2021-02-05 00:03:28 -0500

    Using @lekoarts/gatsby-theme-minimal-blog template

--- TODO.md
+++
@@ -1,3 +0,0 @@
-- Work on MDX Renderer styles / components
-
-- Use tailwind.css, transition away from theme-ui
--- src/pages/about.tsx
+++
@@ -1,8 +0,0 @@
-import { PageProps } from "gatsby"
-import React from "react"
-
-const about: React.FC<PageProps> = ({}) => {
-  return <></>
-}
-
-export default about
